e981b279dccd464d884b3209715f8b8aaa833173
forfolias <forfolias@gmail.com>
2025-01-06 22:56:13 +0200

    List only available UI libraries

--- tax/ui/apps/__init__.py
+++ tax/ui/apps/__init__.py
@@ -1,9 +1,14 @@
-from tax.ui.apps.graphical_ui import GraphicalUi
-from tax.ui.apps.interactive_shell_ui import InteractiveShellUi
+import importlib.util
+
 from tax.ui.apps.cli_ui import CliUi
 
-available_user_interfaces = (
-    CliUi,
-    InteractiveShellUi,
-    GraphicalUi,
-)
+available_user_interfaces = [CliUi]
+
+if importlib.util.find_spec("beaupy") is not None:
+    from tax.ui.apps.interactive_shell_ui import InteractiveShellUi
+    available_user_interfaces.append(InteractiveShellUi)
+
+if importlib.util.find_spec("tkinter") is not None:
+    from tax.ui.apps.graphical_ui import GraphicalUi
+    available_user_interfaces.append(GraphicalUi)
+
